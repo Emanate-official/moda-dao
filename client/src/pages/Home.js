3de795f8a0f2aa64e94eafa12836f3bd0034936c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-04-19 09:26:34 +1000

    Chore: Roll back home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,12 +14,9 @@ const Home = () => {
       <div className="container-fluid py-5">
         <div className="row">
           <div className="col">
-            <Card>
-            </Card>
-            <Card>
-            </Card>
-            <Card>
-            </Card>
+            <h1>Moda DAO</h1>
+            <h2>Consectetur labore fugiat reprehenderit reprehenderit esse aute anim dolor nisi tempor commodo minim laborum velit.</h2>
+            <p>Labore Lorem tempor irure velit consequat laborum Lorem dolore. Adipisicing culpa laborum duis amet consectetur ex reprehenderit mollit ad. Irure ea pariatur reprehenderit incididunt irure anim nostrud commodo et. Incididunt et eu laborum dolore voluptate cupidatat ad labore id aute anim enim. Proident officia eu laboris esse id nostrud nulla amet commodo exercitation irure. Duis sit ut tempor nisi reprehenderit anim veniam culpa fugiat esse minim qui labore. Lorem esse laboris do ullamco et exercitation eu et.</p>
           </div>
           <div className="col">
             <Card>
