db153bed830f68cd2a14851a8a699b79e088bf53
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-04-19 13:43:02 +1000

    Feat: Add stats component

---
+++ client/src/components/Stats.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+// import Web3 from "web3";
+// import ERC20 from "../build/contracts/ERC20.json";
+
+const Stats = () => {
+    const [seconds, setSeconds] = useState(0);
+    const [nfts, setNFTs] = useState(0);
+    const [songs, setSongs] = useState(0);
+    const [memebers, setMembers] = useState(0);
+
+//   console.log(process.env.REACT_APP_CONTRACT_ADDRESS);
+
+//   const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.REACT_APP_NODE));
+
+//   const contract = new web3.eth.Contract(
+//     ERC20.abi,
+//     process.env.REACT_APP_CONTRACT_ADDRESS
+//   );
+
+//   const loadSupply = async () => {
+//     const result = await contract.methods.count().call();
+//     console.log(result);
+//     setSupply(result);
+//   }
+
+//   loadSupply();
+
+  return (
+      <div>
+        <h2>Stats</h2>
+        <p>Seconds of audio streamed</p>
+        <p>NFTs traded</p>
+        <p>Songs on chain</p>
+        <p>DAO members</p>
+      </div>
+  );
+};
+
+export default Stats;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Card from "../components/Card";
+import Stats from "../components/Stats";
 
 const Home = () => {
   const links = [
-    { label: "About", route: "/stake" },
+    { label: "About", route: "/about" },
     { label: "Partners", route: "/partners" },
     { label: "People", route: "/people" }
   ];
@@ -18,9 +19,11 @@ const Home = () => {
             <h2>Consectetur labore fugiat reprehenderit reprehenderit esse aute anim dolor nisi tempor commodo minim laborum velit.</h2>
             <p>Labore Lorem tempor irure velit consequat laborum Lorem dolore. Adipisicing culpa laborum duis amet consectetur ex reprehenderit mollit ad. Irure ea pariatur reprehenderit incididunt irure anim nostrud commodo et. Incididunt et eu laborum dolore voluptate cupidatat ad labore id aute anim enim. Proident officia eu laboris esse id nostrud nulla amet commodo exercitation irure. Duis sit ut tempor nisi reprehenderit anim veniam culpa fugiat esse minim qui labore. Lorem esse laboris do ullamco et exercitation eu et.</p>
           </div>
+        </div>
+        <div className="row">
           <div className="col">
-            <Card>
-            </Card>
+            <Stats>
+            </Stats>
           </div>
         </div>
       </div>
